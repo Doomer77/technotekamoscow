b11939bdefdcb5dc01fa2bdfeed065f68c4b4a5a
Doomer77 <lfa1925@rambler.ru>
2023-03-15 11:17:58 +0300

    init app Technoteka Moscow add title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <title>Technoteka Moscow</title>
+      </Head>
       <body>
         <Main />
         <NextScript />

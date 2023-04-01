7d066e0e6b4b2d1eadde71b87d75819b21c953bc
Doomer77 <lfa1925@rambler.ru>
2023-04-01 21:17:27 +0300

    add functional change icon bureger

--- components/modules/header/Header.tsx
+++ components/modules/header/Header.tsx
@@ -4,14 +4,17 @@ import { Logo } from '../../logo/Logo'
 import { FeedBack } from '../../feedback/FeedBack'
 import styles from './Header.module.css'
 import { Burger } from '../../burger/Bureger'
+import { useState } from 'react'
 
 export const Header = ({ ...props }: HeaderProps): JSX.Element => {
+  const [nav, setNav] = useState(false)
+
   return (
     <header className={styles.header} {...props}>
       <Logo />
       <NavHeader />
       <FeedBack />
-      <Burger nav={true} />
+      <Burger nav={nav} onClick={() => setNav(!nav)} />
     </header>
   )
 }

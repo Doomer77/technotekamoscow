51ce89b0577339a9cb31f394fbab5a58f575efa0
Doomer77 <lfa1925@rambler.ru>
2023-07-23 14:23:32 +0300

    finished dev website dev page in desctop

--- pages/website-dev.tsx
+++ pages/website-dev.tsx
@@ -6,6 +6,7 @@ import { Card } from '../components/ui/card/Card'
 import { cards } from '../utils/cards'
 import devImage from '../assets/images/png/dev-image.png'
 import styles from '../styles/Dev.module.scss'
+import { Button } from '../components/ui/button/Button'
 
 function WebSiteDev() {
   return (
@@ -100,9 +101,71 @@ function WebSiteDev() {
             </div>
           </div>
         </div>
+
+        <div className={styles.info}>
+          <div className={styles.info__seo}>
+            <div className={styles.info__seo_card}>
+              <h2 className={styles.info__card__heading}>
+                SEO продвижение сайта
+              </h2>
+              <p className={styles.info__card__text}>
+                Стратегия раскрутки зависит от множества факторов:
+              </p>
+              <ul className={styles.info__card__list}>
+                <li className={styles.info__card__item}>‌- Тематика сайта</li>
+                <li className={styles.info__card__item}>
+                  ‌- Активность конкурентов
+                </li>
+                <li className={styles.info__card__item}>
+                  ‌- Бюджет продвижения
+                </li>
+                <li className={styles.info__card__item}>- ‌Контент</li>
+                <li className={styles.info__card__item}>‌- Сроки</li>
+                <li className={styles.info__card__item}>
+                  ‌- Продвигаемые запросы
+                </li>
+                <li className={styles.info__card__item}>
+                  ‌- Структура сайта и др.
+                </li>
+              </ul>
+              <p className={styles.info__card__text}>
+                Бюджет на SEO продвижение сайта всегда рассчитывается
+                индивидуально.
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.info__support}>
+            <div className={styles.info__support_card}>
+              <h2 className={styles.info__card__heading}>
+                Техническая поддержка
+              </h2>
+              <p className={styles.info__card__text}>
+                Успешный проект — постоянно обновляемый проект, быстро
+                реагирующий на меняющийся рынок.
+              </p>
+              <p className={styles.info__card__text}>
+                Поддержка только нами выполненных проектов
+              </p>
+              <p className={styles.info__card__text}>
+                Мы оказываем комплексное сопровождение сайтов, а также выполняем
+                разовые задачи
+              </p>
+              <p className={styles.info__card__text}>Цена от 5000₽</p>
+              <Button>Заказать</Button>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
 }
 
 export default withLayout(WebSiteDev, 'layout')
+// ‌- Тематика сайта;
+// ‌- Активность конкурентов;
+// ‌- Бюджет продвижения;
+// - ‌Контент;
+// ‌- Сроки;
+// ‌- Продвигаемые запросы;
+// ‌- Структура сайта и др.
